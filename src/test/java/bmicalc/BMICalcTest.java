d1d6b3b55cab01b6574bbe8140d4db832b6737dc
Raúl Castro <raulcastrov@uma.es>
2023-02-24 13:47:28 +0100

    implementacion tests cuando masa o altura son menores que cero

--- src/test/java/bmicalc/BMICalcTest.java
+++ src/test/java/bmicalc/BMICalcTest.java
@@ -1,8 +1,9 @@
 package bmicalc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BMICalcTest {
@@ -13,5 +14,27 @@ private BMICalcImpl calc = new BMICalcImpl();
 		double mass = 50;
 		double height = 1.70;
 		assertEquals(17.30, (double)Math.round(calc.bmi(mass, height)*100)/100);
+	}
+	
+	@Test
+	public void bmiNegativeMass() {
+		double mass = -50;
+		double height = 1.70;
+		
+		Exception exception = assertThrows(IllegalArgumentException.class, () -> calc.bmi(mass, height));
+		String expectedMessage = "Mass and height must be higher than 0";
+	    String actualMessage = exception.getMessage();
+	    assertTrue(actualMessage.contains(expectedMessage));
+	}
+	
+	@Test
+	public void bmiNegativeHeight() {
+		double mass = 50;
+		double height = -1.70;
+		
+		Exception exception = assertThrows(IllegalArgumentException.class, () -> calc.bmi(mass, height));
+		String expectedMessage = "Mass and height must be higher than 0";
+	    String actualMessage = exception.getMessage();
+	    assertTrue(actualMessage.contains(expectedMessage));
 	}
 }
