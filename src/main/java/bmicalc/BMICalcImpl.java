7bef7b892b649e49695b9d1e07f329fcd3f7ed96
Raúl Castro <raulcastrov@uma.es>
2023-02-24 13:42:55 +0100

    implementacion metodo category

--- src/main/java/bmicalc/BMICalcImpl.java
+++ src/main/java/bmicalc/BMICalcImpl.java
@@ -16,7 +16,18 @@ public class BMICalcImpl implements BMICalc {
 	}
 
 	public String category(double bmi) {
-		return "";
+		if (bmi<=0)
+			throw new IllegalArgumentException("BMI must be higher than 0");
+		
+		if (bmi < 18.5) {
+			return "Underweight";
+		} else if (bmi < 25) {
+			return "Normal";
+		} else if (bmi < 30) {
+			return "Overweight";
+		} else {
+			return "Obese";
+		}
 	}
 
 	public boolean abdominalObesity(double waistCircumference, char gender) {
