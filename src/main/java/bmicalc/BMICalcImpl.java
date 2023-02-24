273815e1f0129dcc93aa4e546a7b260f2757d127
Raúl Castro <raulcastrov@uma.es>
2023-02-24 13:42:14 +0100

    implementacion metodo bmi

--- src/main/java/bmicalc/BMICalcImpl.java
+++ src/main/java/bmicalc/BMICalcImpl.java
@@ -3,7 +3,16 @@ package bmicalc;
 public class BMICalcImpl implements BMICalc {
 
 	public double bmi(double mass, double height) {
-		return 0.0;
+		if (mass<=0 || height<=0)
+			throw new IllegalArgumentException("Mass and height must be higher than 0");
+		if (mass>640)
+			throw new IllegalArgumentException("Mass must be lower than 640");
+		if (height>275)
+			throw new IllegalArgumentException("Height must be lower than 275");
+		
+		double bmi = (mass/(height*height));
+		
+		return bmi;
 	}
 
 	public String category(double bmi) {
