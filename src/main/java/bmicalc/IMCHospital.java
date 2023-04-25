aa4cdcbfd0f247a93bc7daaed5b880a4d9ec3da8
Raúl Castro <raulcastrov@uma.es>
2023-04-25 21:01:26 +0200

    Implementacion de interfaz para hospital

---
+++ src/main/java/bmicalc/IMCHospital.java
@@ -0,0 +1,11 @@
+package bmicalc;
+
+import java.util.Map;
+
+public interface IMCHospital {
+	
+	public Map<Double, String> imc(double altura, double peso);
+	
+	public boolean tieneObesidadAbdominal(char genero, double circunferencia);
+	
+}
